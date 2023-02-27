5058cc3446578d6d78e31068bf52b167b003d5b6
tekater <120014627+tekater@users.noreply.github.com>
2023-02-27 19:52:00 +0900

    Create example2.cpp

---
+++ example2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  
+    std::cout << "Name\t\t\b: \"The war and the peace\"";
+    std::cout << "\nAvtor\t\t\b: L.N. Tolstoj";
+    std::cout << "\nIzdatelstvo: Piter";
+    std::cout << "\nPages\t\t\:500\n";
+  
+    std::cout << "\n“U lukomor’a dub zelenij,";
+    std::cout << "\nZlataya zep na dupe tom,";
+    std::cout << "\nI dnem i nochju kot uchenij";
+    std::cout << "\nvse hodit po cepi krugom”";
+  
+    std::cout <<"\n=============================";
+    std::cout <<"\n|\t\t\t\t\t\t\t|";
+    std::cout <<"\n|\t\tVremena goda\t\t|";
+    std::cout <<"\n|\t\t\t\t\t\t\t|";
+    std::cout <<"\n=============================";
+    std::cout <<"\n|ZIMA | VESNA | LETO | OSEN |";
+    std::cout <<"\n=============================";
+}
