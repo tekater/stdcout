f6bd3a88e5bb5c2d348bb07976137a9abb210b90
tekater <120014627+tekater@users.noreply.github.com>
2023-02-27 19:51:40 +0900

    Create example1.cpp

---
+++ example1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!\n \n";
+    
+    std::cout << "\\\\Dennis Ritchi\\\\ \n";
+    std::cout << "";
+    std::cout << "\nTo be, or not to be: that is the";
+    std::cout << "question:\nWhether `tis nobler";
+    std::cout << " in the mind to suffer\nThe slings and";
+    std::cout << " arrows of outrageous fortune,";
+    std::cout << "\nOr to take arms against a sea of";
+    std::cout << " troubles,\nAnd by opposing end them?";
+    std::cout << "\nWilliam Shakespeare";
+}
