21accd5004c6627f7b2ac2fbb51ddb78d1a5eb78
tekater <120014627+tekater@users.noreply.github.com>
2023-03-01 15:46:51 +0900

    Update Control1.cpp

--- Control1.cpp
+++ Control1.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Задача 1-2:\n";
-    int a,b,c;
-  std::cout << "\nВведите первое число:\n";
+    std::cout << "Task 1-2:\n"; // Задача 1-2
+    int a, b, c;
+    std::cout << "\nEnter the first number:\n"; // Введите первое число
     std::cin >> a;
-  std::cout << "Введите второе число:\n";
+    std::cout << "Enter the second number:\n"; // Введите второе число
     std::cin >> b;
-  std::cout << "Введите третье число:\n";
+    std::cout << "Enter the third number:\n"; // Введите третье число
     std::cin >> c;
-    std::cout << "\nСумма: " << a + b + c;
-    std::cout << "\nПроизведение: " << a * b * c;
-    std::cout << "\nСреднее: " << (a + b + c)/2;
+    std::cout << "\nSum: " << a + b + c; // Сумма
+    std::cout << "\nComposition: " << a * b * c; // Произведение
+    std::cout << "\nAverage: " << (a + b + c) / 2; // Среднее
 
-  std::cout << "\nЗадача 3:\n";
-  double cost;
-  int many;
-  double discount;
-  std::cout << "\nВведите стоимость ноутбука:\n";
-  std::cin >> cost;
-  std::cout << "Введите Количество:\n";
-  std::cin >> many;
-  std::cout << "Введите скидку n%:\n";
-  std::cin >> discount;
-  
-  discount = discount / 100;
-  cost = cost - (cost * discount);
-  std::cout << "К оплате:\n" << cost * many;
+    std::cout << "\n\nTask 3:\n";
+    double cost; // цена
+    int quan; // Количество
+    double discount; // Скидка
+    std::cout << "\nEnter the cost:\n"; // Введите стоимость ноутбука
+    std::cin >> cost;
+    std::cout << "Enter the quantity:\n"; // Введите количество
+    std::cin >> quan;
+    std::cout << "Enter a discount n%:\n"; // Введите скидку
+    std::cin >> discount;
+
+    discount = discount / 100;
+    cost = cost - (cost * discount);
+    std::cout << "For Payment:\n" << cost * quan; // К оплате
 }
